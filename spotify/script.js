ec368af74aedd44038500b9dcccda002299b495d
napraveen <113528770+napraveen@users.noreply.github.com>
2022-11-27 11:54:03 +0530

    Update script.js

--- spotify/script.js
+++ spotify/script.js
@@ -1,3 +1,14 @@
-function newelem() {
-  
-  }
+function newelem(){
+
+const page = window.open("playlist.html");
+page.addEventListener('DOMContentLoaded',()=>{
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("sg-name-2").innerHTML;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    console.log(li);
+    page.document.getElementById("myul").appendChild(li);
+
+})
+
+}
